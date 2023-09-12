ac7349cd7ddd0bd032d45c13ae304d56889a7395
Loveleen Singh <lavi.cgc@gmail.com>
2023-09-12 12:17:48 +0530

    HomePage Movies Lists added

--- src/components/VideoBg.jsx
+++ src/components/VideoBg.jsx
@@ -13,8 +13,6 @@ const VideoBg = ({ movieId }) => {
         <>
             <iframe className="w-[100%] aspect-video" src={`https://www.youtube.com/embed/${trailerVideo?.key}?&autoplay=1&mute=1`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share ">
             </iframe >
-
-
         </>
     )
 }
